3a8b220c0254b966eca17c4e964962cf6d2f1317
Abubkr Hago <24502430+abubkr-hago@users.noreply.github.com>
2024-06-24 08:54:20 +0400

    fix: add to cart has no user set

--- cloud/main.ts
+++ cloud/main.ts
@@ -1,4 +1,11 @@
 // It is best practise to organize your cloud functions group into their own file. You can then import them in your main.js.
 await import('./functions.js');
 
+Parse.Cloud.beforeSave('Cart', ({ object, user, master }) => {
+  if (!master && !user)
+    throw new Parse.Error(Parse.Error.INVALID_SESSION_TOKEN, 'Please sign in first.');
+  if (user)
+    object.set('user', user);
+});
+
 export default void 0;
